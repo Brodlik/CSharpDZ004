bc0778c00b1ff6deeb2ca3f30848b4c6a379e3d9
Svetlana <brodlik@me.com>
2022-08-05 17:31:35 +0300

    Выведение массива из 8 элементов, вариант

---
+++ Task029_Var2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. 
+// (вводим массив с клавиатуры (т. е. через консоль)
+
+Console.Write ("Введите число A: ");
+int A = Convert.ToInt32 (Console.ReadLine());
+
+Console.Write ("Введите число B: ");
+int B =Convert.ToInt32 (Console.ReadLine());
+
+Console.Write ("Введите число C: ");
+int C =Convert.ToInt32 (Console.ReadLine());
+
+Console.Write ("Введите число D: ");
+int D =Convert.ToInt32 (Console.ReadLine());
+
+Console.Write ("Введите число E: ");
+int E =Convert.ToInt32 (Console.ReadLine());
+
+Console.Write ("Введите число F: ");
+int F =Convert.ToInt32 (Console.ReadLine());
+
+Console.Write ("Введите число G: ");
+int G =Convert.ToInt32 (Console.ReadLine());
+
+Console.Write ("Введите число H: ");
+int H =Convert.ToInt32 (Console.ReadLine());
+
+Console.WriteLine($"Массив из 8 элементов: [ {A}, {B}, {C}, {D}, {E}, {F}, {G}, {H}]");
